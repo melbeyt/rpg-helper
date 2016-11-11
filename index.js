a4255212e4e9cf7a0b7f6340ebf24ad713817e3f
melbeyt <melanieb@slalom.com>
2016-11-11 09:37:32 -0600

    Got realtime updates across browser windows up

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,7 +15,19 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
+io.on('connection', function(socket) {
+	console.log('a user connected');
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
+
+	socket.on('chat message', function(msg){
+		console.log('message: ' + msg);
+		io.emit('chat message', msg);
+	});
+});
+
+http.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
